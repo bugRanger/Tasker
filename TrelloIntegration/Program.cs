4f6894d61d9623d6c732f78ce3a9a055da23d7d1
bugRanger <departmentofjusticeevil@gmail.com>
2020-05-12 16:48:05 +0700

    Fix access method.

--- TrelloIntegration/Program.cs
+++ TrelloIntegration/Program.cs
@@ -255,7 +255,7 @@
             }
         }
 
-        private static void OnRedmine_UpdateProjects(object sender, Project[] projects)
+        static void OnRedmine_UpdateProjects(object sender, Project[] projects)
         {
             foreach (var project in projects)
             {
